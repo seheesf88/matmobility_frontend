027243472232d70b9b8664b087c61132656b723e
seheesf88 <seheesf88@gmail.com>
2020-01-31 23:12:57 -0800

    setting up

---
+++ src/HomeContainer/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class HomeContainer extends Component {
+  constructor(){
+    super()
+  }
+
+  render(){
+    return(
+      <div>
+        testing
+      </div>
+    )
+  }
+}
+
+export default HomeContainer
